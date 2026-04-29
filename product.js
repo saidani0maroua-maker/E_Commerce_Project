197fbaa5138790f66bba7904347f4230a05b3ffe
sanaa <sanaahamadache@gmail.com>
2026-04-29 23:07:08 +0100

    first commit

---
+++ product.js
@@ -0,0 +1,59 @@
+ let products=[
+{ id:1,
+  title:"Wemen's Dress",
+  price:"3200 DZD",
+  description:"Vintage Brown Square Neck Ruffled Waist Dress | Fit and Flare Style with Contrast Mesh Details, Perfect for Spring/Summer/Fall",
+  colors:
+     [
+       {
+         name:"Brown",
+         mainImage:"photos/b1.jpg",
+         thumbnails:[
+          "photos/b2.jpg",
+          "photos/b3.jpg",
+          "photos/b4.jpg",
+          "photos/b5.jpg",
+          "photos/b6.jpg", 
+         ],
+         sizes:["S","M","L","XL"]
+        },
+       {
+         name:"Black",
+         mainImage:"photos/l1.jpg",
+         thubnails:[
+          "photos/l2.jpg",
+          "photos/l4.jpg",
+          "photos/l5.jpg",
+          "photos/l6.jpg",
+          "photos/l7.jpg",
+          "photos/l8.jpg",
+          "photos/l9.jpg",
+         ],
+         sizes:["S","M","L","XL"]
+        }
+     ]
+} ,
+{ id:2,
+  title:"Elegent Res Halter Dress",
+  price:"4000 DZD",
+  description:" Women's Elegant Halter neck party dress - Formal/semi-formal event gown, non-stretchy fabric, Structured bodice & flowy skirt for galas, proms, weddings - Easy care Machine Washable.",
+  colors:
+     [
+       {
+         name:"Red",
+         mainImage:"photos/r1.jpg",
+         thumbnails:[
+        
+          "photos/r2.jpg",
+         ],
+         sizes:["S","M","L","XL"]
+        },
+       
+     ]
+} 
+
+
+
+
+
+ ];
